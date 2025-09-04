26e2cb6ececb734517eb097f5edbfd116dbcf6fe
MariaMacedo-prog <mariamacedo2xx@gmail.com>
2025-09-03 22:15:28 -0300

    Introduzindo o angular material e finalizando layout de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatCardModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  usuario = {
+    cpf: '',
+    senha: ''
+  }
 
+  constructor() {}
+  ngOnInit(): void {
+
+  }
+
+  login(): void {
+    console.log("login", this.usuario)
+  }
 }
